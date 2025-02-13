5d46f8457da23e190113201abcd208f9720c527d
HARIKRISHNA95504 <patimaharikrishnap@gmail.com>
2025-02-13 16:06:26 +0530

    getting the all leaves using axios

--- kris-community-app/src/pages/admin/Dashboard/leavemanagement/LeaveHistory.jsx
+++ kris-community-app/src/pages/admin/Dashboard/leavemanagement/LeaveHistory.jsx
@@ -1,10 +1,24 @@
 
 import filter from '../../../../assets/filter.png'
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 function LeaveHistory(){
+    const [leaveHistory,setLeaveHistory] = useState([])
+    useEffect(()=>{
+        getAllLeaves()
+    },[])
+    const getAllLeaves= async()=>{
+        try{
+            const response = await axios.get('http://localhost:4000/leaves')
+            setLeaveHistory(response.data)
+        }catch(error){
+            console.log(error)
+        }
+    }
     return(
         <main>
             <div className='p-3 rounded border-none'>
-                <div className="md:ml-2 md:pt-5 bg-white p-2 ">
+                <div className="md:ml-2 md:pt-5 bg-white p-2  "> {/* max-h-96 overflow-y-auto*/}
                     <div className="flex justify-between md:w-5/6 pt-5 ">   
                         <p className='text-lg font-semibold pl-2'>Leave History</p>
                         <div className="flex justify-center items-center space-x-5">
@@ -13,7 +27,7 @@ function LeaveHistory(){
                             <span><button className="rounded px-8 p-1 bg-green-600 cursor-pointer">Export</button></span>
                         </div>
                     </div>
-                    <table className=" rounded md:p-2  text-center ">
+                    <table className=" rounded md:p-2  text-center table-auto ">
                         <thead>
                             <tr>
                                 <th className="border-none  md:p-2 md:px-10 md:text-xl">Name(s)</th>
@@ -26,13 +40,14 @@ function LeaveHistory(){
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className=" bg-none rounded bg-sky-100 ">
-                                <td className="border-none  md:p-auto md:px-auto md:p-3">The Sliding </td>
-                                <td className="border-none  md:p-auto md:px-auto">Malcolm Lockyer</td>
-                                <td className="border-none  md:p-auto md:px-auto">22/04/2022</td>
-                                <td className="border-none  md:p-auto md:px-auto">28/04/2022</td>
-                                <td className="border-none  md:p-auto md:px-auto">Sick</td>
-                                <td className="border-none  md:p-auto md:px-auto">Personal</td>
+                            {leaveHistory.map(leave =>(
+                                <tr className=" bg-none rounded bg-sky-100 ">
+                                <td className="border-none  md:p-auto md:px-auto md:p-3">{leave.firstName+leave.lastName}</td>
+                                <td className="border-none  md:p-auto md:px-auto">{leave.duration}</td>
+                                <td className="border-none  md:p-auto md:px-auto">{leave.startDate}</td>
+                                <td className="border-none  md:p-auto md:px-auto">{leave.endDate}</td>
+                                <td className="border-none  md:p-auto md:px-auto">{leave.leaveType}</td>
+                                <td className="border-none  md:p-auto md:px-auto">{leave.reason}</td>
                                 <td className="border-none  md:p-auto md:px-auto">
                                     <select className="border rounded md:px-10 md:p-1 bg-indigo-950 text-white font-semibold">
                                         <option value="Action" >Action</option>
@@ -41,21 +56,7 @@ function LeaveHistory(){
                                     </select>
                                 </td>
                             </tr>
-                            <tr className=" bg-none rounded  ">
-                                <td className="border-none  md:p-auto md:px-auto md:p-3">The Sliding </td>
-                                <td className="border-none  md:p-auto md:px-auto">Malcolm Lockyer</td>
-                                <td className="border-none  md:p-auto md:px-auto">22/04/2022</td>
-                                <td className="border-none  md:p-auto md:px-auto">28/04/2022</td>
-                                <td className="border-none  md:p-auto md:px-auto">Sick</td>
-                                <td className="border-none  md:p-auto md:px-auto">Personal</td>
-                                <td className="border-none  md:p-auto md:px-auto">
-                                    <select className="border rounded md:px-10 md:p-1 bg-indigo-950 text-white font-semibold">
-                                        <option value="Action" >Action</option>
-                                        <option value="Delete">Delete</option>
-                                        <option value="Edit">Edit</option>
-                                    </select>
-                                </td>
-                            </tr>
+                            ))}
                             
                         </tbody>
                     </table>
